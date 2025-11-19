e681e62e6c8d600724fb1e58993ade2b8226308e
thomas-o-chan <thomas.chan.ac+github@gmail.com>
2025-11-19 21:21:46 +0100

    chore: add basic data validation for player profile info

--- src/app/api/player/[slug]/route.ts
+++ src/app/api/player/[slug]/route.ts
@@ -10,6 +10,9 @@ export async function GET(
       getProfileData(playerId),
       getStatsData(playerId),
     ]);
+
+    assertProfileData(profileData);
+
     const data: PlayerInfo = {
       profile: profileData,
       stats: statsData,
@@ -21,7 +24,8 @@ export async function GET(
       headers: { "Content-Type": "application/json" },
     });
   } catch (error) {
-    console.error(error);
+    const message = error instanceof Error ? error.message : String(error);
+    console.error(message);
     return new Response(
       JSON.stringify({ error: "Failed to fetch player data" }),
       {
@@ -46,4 +50,37 @@ async function getProfileData(playerId: string): Promise<PlayerProfileInfo> {
 async function getStatsData(playerId: string): Promise<PlayerStatsInfo> {
   const res = await fetch(`https://api.chess.com/pub/player/${playerId}/stats`);
   return await res.json();
+}
+
+/** Basic validation for structure of PlayerProfileInfo */
+function assertProfileData(data: unknown): asserts data is PlayerProfileInfo {
+  if (!data || typeof data !== "object") {
+    throw new Error("Invalid profile data: not an object");
+  }
+  const missingProps: string[] = [];
+  const requiredProps = [
+    "player_id",
+    "@id",
+    "url",
+    "username",
+    "followers",
+    "country",
+    "last_online",
+    "joined",
+    "status",
+    "is_streamer",
+    "verified",
+    "league",
+    "streaming_platforms",
+  ];
+  requiredProps.forEach((prop) => {
+    if (!(prop in data)) {
+      missingProps.push(prop);
+    }
+  });
+  if (missingProps.length > 0) {
+    throw new Error(
+      `Invalid profile data: missing properties: ${missingProps.join(", ")}`
+    );
+  }
 }
