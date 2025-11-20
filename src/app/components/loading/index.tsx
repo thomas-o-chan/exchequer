eb43bb8567913b153f65dc17f9a389eca1a45c6c
thomas-o-chan <thomas.chan.ac+github@gmail.com>
2025-11-20 09:10:34 +0100

    feat: better loading spinner

--- src/app/components/loading/index.tsx
+++ src/app/components/loading/index.tsx
@@ -3,7 +3,7 @@ import styles from "./loading.module.css";
 export function Loading() {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.message}>Loading...</div>
+      <div className={styles.message}><div className={styles.spinning}>♜</div></div>
     </div>
   );
 }
